1dcf0d9514012badd30a503c565f40aca55d7329
Erick Perez <erick.perezrayo54@gmail.com>
2022-03-05 18:43:55 -0600

    Inicio

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve:{
+        extensions: ['.js', '.jsx']
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+        {
+            test: /\.html$/,
+            use: [
+                {
+                    loader: 'html-loader',
+                }
+            ]
+        }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ],
+    devServer: {
+        historyApiFallback: true,
+        static: path.join(__dirname, 'dist'),
+        port: 8000
+    },
+    mode: 'development'
+}
